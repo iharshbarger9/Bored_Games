fb6a98a1fbcc2be659da5f93afea60ce8fa5905e
lmac02 <louden.maclay@yahoo.com>
2021-05-02 01:22:09 -0400

    stats page and leaderboard are finished

--- app/src/main/java/com/example/boredgames/StatsActivity.java
+++ app/src/main/java/com/example/boredgames/StatsActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -25,19 +27,75 @@ import org.w3c.dom.Text;
 public class StatsActivity extends AppCompatActivity {
     private static final String TAG = "Stats Activity";
     public static final String DOCUMENT_PATH = "stats/global stats";
+    public static final String COLLECTION_PATH = "stats";
 
+    FirebaseFirestore stats_db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
 
-        //update stats button
-        Button update_button = (Button) findViewById(R.id.update_button);
-        update_button.setOnClickListener(new View.OnClickListener() {
+        //constructing leaderboard
+        TextView first = findViewById(R.id.first_spot);
+        TextView second = findViewById(R.id.second_spot);
+        TextView third = findViewById(R.id.third_spot);
+        TextView fourth = findViewById(R.id.fourth_spot);
+        TextView fifth = findViewById(R.id.fifth_spot);
+
+        stats_db.collection(COLLECTION_PATH)
+                .orderBy("win", Query.Direction.DESCENDING)
+                .limit(5)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            int i = 1;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                switch(i) {
+                                    case 1:
+                                        first.setText(document.getId() + " : " + document.get("win") + "-" + document.get("draw") + "-" + document.get("loss"));
+                                        break;
+                                    case 2:
+                                        second.setText(document.getId() + " : " + document.get("win") + "-" + document.get("draw") + "-" + document.get("loss"));
+                                        break;
+                                    case 3:
+                                        third.setText(document.getId() + " : " + document.get("win") + "-" + document.get("draw") + "-" + document.get("loss"));
+                                        break;
+                                    case 4:
+                                        fourth.setText(document.getId() + " : " + document.get("win") + "-" + document.get("draw") + "-" + document.get("loss"));
+                                        break;
+                                    case 5:
+                                        fifth.setText(document.getId() + " : " + document.get("win") + "-" + document.get("draw") + "-" + document.get("loss"));
+                                        break;
+                                }
+                                i++;
+                            }
+                        } else {
+                            Log.d(TAG, "database query was unsuccessful");
+                        }
+                    }
+                });
+
+        //find player button
+        EditText search_et = findViewById(R.id.hs_search_tb);
+        Button search_bt = findViewById(R.id.hs_search_bt);
+        TextView search_results = findViewById(R.id.search_results);
+        search_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                updateStats();
+                search_et.getText();
+                stats_db.collection(COLLECTION_PATH).document(search_et.getText().toString()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot document) {
+                        if (document.exists()) {
+                            search_results.setText(document.getId() + " : " + document.get("win") + "-" + document.get("draw") + "-" + document.get("loss"));
+                        } else {
+                            search_results.setText("Player not found");
+                        }
+                    }
+                });
             }
         });
 
@@ -45,8 +103,8 @@ public class StatsActivity extends AppCompatActivity {
         //testingFirestore();
     }
 
-    public void updateStats() {
-        FirebaseFirestore stats_db = FirebaseFirestore.getInstance();
+  /*  public void updateStats() {
+        //FirebaseFirestore stats_db = FirebaseFirestore.getInstance();
         stats_db.document(DOCUMENT_PATH).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
@@ -77,5 +135,5 @@ public class StatsActivity extends AppCompatActivity {
                 Log.d(TAG, "failed");
             }
         });
-    }
+    } */
 }
